a4fabe5032dfdbf28a32588880fe8ea0dfc87f87
Joaquin Cardoso <joaquin.cardoso.r@gmail.com>
2024-12-20 17:49:08 +0800

    feature/localStorage for activities

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { activityReducer , initialState } from "./reducers/activityReducer"
 import Form from "./components/Form"
 import ActivityList from "./components/ActivityList"
@@ -7,6 +7,9 @@ function App() {
 
   const [ state, dispatch ] = useReducer(activityReducer, initialState)
 
+  useEffect( () => {
+    localStorage.setItem('activities', JSON.stringify(state.activities))
+  }, [state.activities])
   return (
     <>
       <header className="bg-lime-600 py-3">
